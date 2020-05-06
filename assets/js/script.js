0acf45d8ffaa39b8e87a4513b1ea12413bcd449f
FoxHermit <62595041+FoxHermit@users.noreply.github.com>
2020-05-06 20:40:39 +0200

    Corrigé

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var name = 'Fox';
+var firstname = 'Hermit';
+var city = 'Tokyo';
+alert('Nom : ' + name + '\n' + 'Prénom : ' + firstname + '\n' + 'Ville : ' + city);
+// Le \ permet de délimiter pour faire apparaître les apostrophes sans se casser le cul avec les quotes, exemples : alert('Je m\appelle ') ça s'appelle échapper une quote, et on peut mettre des espaces entre les quotes
